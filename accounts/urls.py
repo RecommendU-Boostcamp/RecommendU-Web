892612572db9fb6f4db83960fb0ad96cf7a28bc9
Jangyunseong <dbstjd0924@gmail.com>
2023-01-18 09:16:38 +0000

    feat : initialize login and signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
-
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/',views.login_test),
+    path('signup/',views.signup_test),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from services.models import JobList,MajorList
 
 # Create your views here.
+
+def login_test(request):
+    context = {"context":"hello"}
+    return render(request, 'login.html',context)
+
+def signup_test(request):
+    jobquery = JobList.objects.all()
+    majorquery = MajorList.objects.all()
+    for i in range(0,len(majorquery)):
+        majorquery[i].id = "job_"+str(i)
+        majorquery[i].major_small = majorquery[i].major_small.split(',')
+    context = {
+        "joblist":jobquery,
+        "majorlist" : majorquery,
+    }
+    return render(request, 'signup.html',context)
